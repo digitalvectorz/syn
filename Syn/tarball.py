2bd62680eea2422458bab52562298121dc1e1f6c
Paul R. Tagliamonte <paultag@ubuntu.com>
2011-07-12 17:39:26 -0400

    adding in some tarball tests

---
+++ Syn/tarball.py
@@ -0,0 +1,6 @@
+# Copyright 2011 (c) GNU GPL-3+, Paul Tagliamonte <paultag@gmail.com>
+
+class tarball:
+	def __init__(self, tarball):
+		self.tarball = tarball
+		Syn.log.l(Syn.log.VERBOSE, "attempting to load " + archive )
---
+++ tests/test-cases/tarball-test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# Copyright 2011 (c) GNU GPL-3+, Paul Tagliamonte <paultag@gmail.com>
+
+import Syn.tarball
+
+test_tarball = "test-1.0.tar.gz"
+
+t = Syn.tarball.tarball(test_tarball)
+
