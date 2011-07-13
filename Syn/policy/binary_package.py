2f7267ca548396cb8dbb14ac8920ea0d735d9838
Paul R. Tagliamonte <paultag@ubuntu.com>
2011-07-13 14:39:50 -0400

    Adding in binary package policy basics

---
+++ Syn/policy/binary_package.py
@@ -0,0 +1,20 @@
+# Copyright 2011 (c) GNU GPL-3+, Paul Tagliamonte <paultag@gmail.com>
+
+#syn/
+#  filesums <-- gzip compressed
+#  metafile <-- gzip compressed JSON
+#  hooks:
+#    preinst
+#    postinst
+#    prerm
+#    postrm
+#  root:
+#    usr...
+#    etc...
+
+TOP_LEVEL_DIR = "syn"
+FILESUMS      = "filesums"
+METAFILE      = "metafile"
+HOOK_DIR      = "hooks"
+FS_ROOT       = "root"
+
