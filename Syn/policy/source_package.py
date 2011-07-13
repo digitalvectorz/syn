0f0bd5b48f02ac2ba4a7d485bd2a9730bb11ff64
Paul R. Tagliamonte <paultag@ubuntu.com>
2011-07-13 14:42:09 -0400

    adding in source basics

---
+++ Syn/policy/source_package.py
@@ -0,0 +1,18 @@
+# Copyright 2011 (c) GNU GPL-3+, Paul Tagliamonte <paultag@gmail.com>
+
+#package-N.M
+#  synd/
+#    hooks/
+#      preinst
+#      postinst
+#      prerm
+#      postrm
+#    build # makefile to build source
+#    build.env # env vars in JSON
+#    pkgmeta # metafile for package details (see A2)
+
+SOURCE_DIRECTORY = "synd"
+BUILDFILE        = "build"
+ENVFILE          = "build.env"
+METAFILE         = "metapkg"
+
