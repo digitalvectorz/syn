1dbc80405b4d72218f9466d4e3207fea6b155393
Ryan Maloney <rpm5779@rit.edu>
2011-07-14 23:51:27 -0400

    Fixed trailing white space, style parts, and made a more generalized file start point for md5_json_test class also made a delta.py for use in testing since we use dicts and comparing dicts a lot.

--- Syn/md5_json.py
+++ Syn/md5_json.py
@@ -6,26 +6,26 @@ import os.path
 
 #Remaking of the old verficiation md5sum function.
 def md5sum(path):
-	m=hashlib.md5()
-	if path== '-':
+	m = hashlib.md5()
+	if path == '-':
 		
 		while True:
-			d=sys.stdin.read(1024)
+			d = sys.stdin.read(1024)
 			if not d:
 				break
 			m.update(d)
-		ret=m.hexdigest()
+		ret = m.hexdigest()
 	else:
 		try:
-			pfile=file(path,'rb')
+			pfile = file(path,'rb')
 		except:
 			return 'File open failed'
 		while True:
-			d=pfile.read(1024)
+			d = pfile.read(1024)
 			if not d:
 				break
 			m.update(d)
-		ret=m.hexdigest()
+		ret = m.hexdigest()
 	return ret
 #recursive function to go through file path and make md5sums of stuff thats not dirs
 def md5sumwd(check):
@@ -47,10 +47,9 @@ def md5sumwd(check):
 def makejsonbfile(filepath):
 	
 	file_id = "json_bfile-testjson.testdb"
-	ret=md5sumwd(filepath)
-	jbfile =Syn.json_bfile.json_bfile(file_id)
+	ret = md5sumwd(filepath)
+	jbfile = Syn.json_bfile.json_bfile(file_id)
 	jbfile.setContent(ret)
 	jbfile.write()
 	
 	return jbfile
-
--- tests/test-cases/md5_json_test.py
+++ tests/test-cases/md5_json_test.py
@@ -5,22 +5,22 @@ import Syn.md5_json
 import os.path
 import delt
 
-test_dir=os.path.realpath("md5json")
+cur_dir=os.path.abspath(os.getcwd())
 
 
-test_dict={
-	 "/home/ryan/syn/tests/md5json/iamadur/iamasubfile":"bf27e69237a4ad56e9589091c47bbc23",
-	   "/home/ryan/syn/tests/md5json/iamafile1":"628bcee1e2cf5ae134866c5683631ff0",
-	   "/home/ryan/syn/tests/md5json/iamafile2":"95ebe25eb6cc5ac8e2a2edcb339ca74b",
-	   "/home/ryan/syn/tests/md5json/iamafile3":"58d5cb7865af03f2f7aeede04ef6acde"
+test_dir=os.path.relpath("md5json",cur_dir)
+
+
+test_dict={"md5json/iamadur/iamasubfile":"bf27e69237a4ad56e9589091c47bbc23",
+	   "md5json/iamafile1":"628bcee1e2cf5ae134866c5683631ff0",
+	   "md5json/iamafile2":"95ebe25eb6cc5ac8e2a2edcb339ca74b",
+	   "md5json/iamafile3":"58d5cb7865af03f2f7aeede04ef6acde"
 	   }
+
+
 
 
 result=Syn.md5_json.makejsonbfile(test_dir)
 compare_content=result.getContent()
 
-print compare_content
-
 assert delt.delt(compare_content, test_dict) == {}
-
-
