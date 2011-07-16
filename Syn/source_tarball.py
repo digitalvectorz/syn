021b56c376605cc06458291b062023e83903a98f
Paul R. Tagliamonte <paultag@ubuntu.com>
2011-07-15 23:06:24 -0400

    adding in sourceball verification

--- Syn/source_tarball.py
+++ Syn/source_tarball.py
@@ -8,8 +8,12 @@ import Syn.policy.source_package as S
 
 class source_tarball(Syn.tarball.tarball):
 	def verify(self):
+		root_bullshit = self.getRootFolder()
 		assert_exists = []
 		for x in S.SOURCE_DIR_REQ_FILES:
-			assert_exists.append(S.SOURCE_DIRECTORY + "/" + x)
+			assert_exists.append(root_bullshit + "/" + S.SOURCE_DIRECTORY + "/" + x)
 		for y in assert_exists:
 			fd = self.readMember(y)
+			crap = fd.read()
+			if crap != "":
+				Syn.log.l(Syn.log.PEDANTIC,"Exists: %s" % y)
