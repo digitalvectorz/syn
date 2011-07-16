0c12c99094cd7b8bfac15b9c7a109578b54b3c41
Paul R. Tagliamonte <paultag@ubuntu.com>
2011-07-15 23:10:50 -0400

    adding in more test coverage

--- Syn/source_tarball.py
+++ Syn/source_tarball.py
@@ -1,10 +1,11 @@
 # Copyright 2011 (c) GNU GPL-3+, Paul Tagliamonte <paultag@gmail.com>
 
 import Syn.log
-import tarfile
-
 import Syn.tarball
+import Syn.exceptions
 import Syn.policy.source_package as S
+
+import tarfile
 
 class source_tarball(Syn.tarball.tarball):
 	def verify(self):
@@ -13,7 +14,10 @@ class source_tarball(Syn.tarball.tarball):
 		for x in S.SOURCE_DIR_REQ_FILES:
 			assert_exists.append(root_bullshit + "/" + S.SOURCE_DIRECTORY + "/" + x)
 		for y in assert_exists:
-			fd = self.readMember(y)
-			crap = fd.read()
-			if crap != "":
-				Syn.log.l(Syn.log.PEDANTIC,"Exists: %s" % y)
+			try:
+				fd = self.readMember(y)
+				crap = fd.read()
+				if crap != "":
+					Syn.log.l(Syn.log.PEDANTIC,"Exists: %s" % y)
+			except KeyError as e:
+				raise Syn.exceptions.SynFormatException("Bad source tarball")
--- tests/test-cases/source-tarball-test.py
+++ tests/test-cases/source-tarball-test.py
@@ -6,4 +6,8 @@ import Syn.source_tarball
 
 stb = Syn.source_tarball.source_tarball("test-1.0.syn.tar.gz")
 
-
+try:
+	stb2 = Syn.source_tarball.source_tarball("test-1.0-bad.syn.tar.gz")
+	assert True == False
+except Syn.exceptions.SynFormatException as e:
+	print "Failed with glee"
