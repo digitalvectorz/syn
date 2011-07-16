6d1a8bd386a69ff3c3640befdba09c1cf663a1ca
Paul R. Tagliamonte <paultag@ubuntu.com>
2011-07-15 23:36:16 -0400

    fixing up test suite

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,25 +1,3 @@
 #!/bin/bash
 
-TSTS=`ls test-cases | grep test`
-
-echo "" > test.log
-
-for x in $TSTS; do
-	echo -n "$x"
-	./test-cases/$x >> test.log
-	ret=$?
-	if [ $ret -ne 0 ]; then
-		echo "	[ [31mFAIL![0m ]"
-		FAIL_FLIP=Fsac
-	else
-		echo "	[ [34mOK[0m ]"
-	fi
-done 
-
-rm *.testdb
-rm -rf test-0.1
-
-if [ "x$FAIL_FLIP" == "x" ]; then
-	echo "No failures. Removing log."
-	rm test.log
-fi
+./tests-ugly.sh | column -t
---
+++ tests/tests-ugly.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+TSTS=`ls test-cases | grep test`
+
+echo "" > test.log
+
+for x in $TSTS; do
+	echo -n "$x"
+	./test-cases/$x >> test.log
+	ret=$?
+	if [ $ret -ne 0 ]; then
+		echo "	[31mFAIL![0m"
+		FAIL_FLIP=Fsac
+	else
+		echo "	[34mOK[0m"
+	fi
+done 
+
+rm *.testdb
+rm -rf test-0.1
+rm write_file
+
+if [ "x$FAIL_FLIP" == "x" ]; then
+	#echo "No failures. Removing log."
+	rm test.log
+fi
