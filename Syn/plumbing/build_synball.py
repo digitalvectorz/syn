62b1fd553d56c0463371248b618e3704f2173234
Paul Tagliamonte <paultag@ubuntu.com>
2011-07-20 01:03:20 +0000

    bitchen

--- Syn/plumbing/build_synball.py
+++ Syn/plumbing/build_synball.py
@@ -7,8 +7,13 @@ import Syn.sh
 PLUMBING_NAME = "build_synball"
 
 def run(args):
-	if Syn.sh.xists(args[2]):
-		Syn.synd.build(args[2])
+	try:
+		if Syn.sh.xists(args[2]):
+			Syn.synd.build(args[2])
+		else:
+			raise Syn.exceptions.SynShittyPlumbingException("Synball does not exist!: %s" % args[2])
+	except IndexError as e:
+		raise Syn.exceptions.SynShittyPlumbingException("You forgot an argument!: %s" % str(e))
 
 Syn.plumber.registerRoute(PLUMBING_NAME, run)
 
