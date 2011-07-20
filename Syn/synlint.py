b87f97d7472f7e82b7b91ac90dbdd41e0a4cd677
Paul Tagliamonte <paultag@ubuntu.com>
2011-07-20 01:41:39 +0000

    metasyn stuffs

--- Syn/synlint.py
+++ Syn/synlint.py
@@ -5,6 +5,7 @@ import Syn.policy.metafile
 def evaluateMetadict(crap):
 	errors   = 0
 	warnings = 0
+	pendent  = 0
 
 	neededTags = Syn.policy.metafile.META_REQUIRED
 	for needed_tag in neededTags:
@@ -14,9 +15,24 @@ def evaluateMetadict(crap):
 			errors = errors + 1
 			Syn.log.l(Syn.log.CRITICAL,"Missing critical tag: %s!!!" % needed_tag)
 
-	Syn.log.l(Syn.log.CRITICAL,"")
+	warningTags = Syn.policy.metafile.META_NEEDED
+	for warning_tag in warningTags:
+		try:
+			result = crap[warning_tag]
+		except KeyError as e:
+			warnings = warnings + 1
+			Syn.log.l(Syn.log.HIGH,"Missing needed tag: %s!!!" % warning_tag)
+
+	goodToHaveTags = Syn.policy.metafile.META_GOODTOHAVE
+	for tag in goodToHaveTags:
+		try:
+			result = crap[tag]
+		except KeyError as e:
+			pendent = pendent + 1
+			Syn.log.l(Syn.log.NOTICE, "Missing pendent tag: %s." % tag)
+
 	Syn.log.l(Syn.log.CRITICAL,"")
 	Syn.log.l(Syn.log.CRITICAL,"Errors:   %s" % errors)
 	Syn.log.l(Syn.log.CRITICAL,"Warnings: %s" % warnings)
-	Syn.log.l(Syn.log.CRITICAL,"")
+	Syn.log.l(Syn.log.CRITICAL,"Pendants: %s" % pendent)
 	Syn.log.l(Syn.log.CRITICAL,"")
