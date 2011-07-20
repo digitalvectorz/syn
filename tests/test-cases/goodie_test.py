cfa40aa6742e4b400b4ef15b29ed8fcd9a7e22f0
Ryan Maloney <rpm5779@rit.edu>
2011-07-20 18:04:54 -0400

    commiting the test for the goodie getter changed a lot of things because it suddenly stopped working

--- tests/test-cases/goodie_test.py
+++ tests/test-cases/goodie_test.py
@@ -3,14 +3,16 @@
 
 import Syn.goodie_getta as getta
 
+test=getta.get_goodies(["/usr/bin","/etc"])
+test2=getta.get_goodies("/usr/bin")
 
-assert len(getta.get_goodies("/usr/bin/"))==1
 
-assert len(getta.get_goodies(["/usr/bin","/etc"]))==2
+assert len(test2)==1
 
-assert  getta.get_goodies("/usr/bin/")[0].has_key("bin")
-
-test=getta.get_goodies(["/usr/bin/","/etc"])
+assert len(test)==2
 
 assert test[0].has_key("bin")
+
 assert test[1].has_key("etc")
+
+assert test2.has_key("bin")
