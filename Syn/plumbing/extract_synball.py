0e727c6ff5920a6f901dc720c934a1bdc409b296
Paul Tagliamonte <paultag@ubuntu.com>
2011-07-21 00:27:37 +0000

    adding in an extract directive

---
+++ Syn/plumbing/extract_synball.py
@@ -0,0 +1,18 @@
+# Copyright 2011 (c) GNU GPL-3+, Paul Tagliamonte <paultag@gmail.com>
+
+import Syn.plumber
+
+PLUMBING_NAME = "extract-synball"
+
+def run(args):
+	try:
+		if Syn.sh.xists(args[2]):
+			stb = Syn.source_tarball.source_tarball(args[2])
+			stb.extractall()
+		else:
+			raise Syn.exceptions.SynShittyPlumbingException("Synball does not exist!: %s" % args[2])
+	except IndexError as e:
+		raise Syn.exceptions.SynShittyPlumbingException("You forgot an argument!: %s" % str(e))
+
+Syn.plumber.registerRoute(PLUMBING_NAME, run)
+
