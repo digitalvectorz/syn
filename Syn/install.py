9ba5f15c6dda02933b7549e2a7e91b1a3d202120
Paul Tagliamonte <paultag@ubuntu.com>
2011-07-24 17:40:28 +0000

    install crap

---
+++ Syn/install.py
@@ -0,0 +1,22 @@
+# Copyright 2011 (c) GNU GPL-3+, Paul Tagliamonte <paultag@gmail.com>
+
+import Syn.exceptions
+import Syn.policy.db as D
+import Syn.package_registry
+
+def install(synball):
+	ROOT_PATH = D.DB_ROOT
+	pkgdb = Syn.package_registry.package_registry(ROOT_PATH)
+
+	try:
+		dbinf = Syn.binary_tarball.binary_tarball(synball)
+		package = dbinf.get_metablob()
+		Syn.log.l(Syn.log.VERBOSE,"Package is name: %s" % package['package'])
+		try:
+			pkgid = pkgdb.getPackage(package['package'])
+			Syn.log.l(Syn.log.PEDANTIC,"Package DB Dump: %s" % pkgid)
+		except Syn.exceptions.PackageNotFoundException as e:
+			Syn.log.l(Syn.log.VERBOSE,"New package install!")
+	except IndexError as e:
+		raise Syn.exceptions.SynShittyPlumbingException("You forgot an argument!: %s" % str(e))
+
