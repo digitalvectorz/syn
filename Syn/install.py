52e8d447ad28e6f8b0d4c2a40a886d39556e80c2
Paul Tagliamonte <paultag@ubuntu.com>
2011-07-28 15:42:15 +0000

    documenting install

--- Syn/install.py
+++ Syn/install.py
@@ -1,3 +1,6 @@
+"""
+Simple install routines
+"""
 # Copyright 2011 (c) GNU GPL-3+, Paul Tagliamonte <paultag@gmail.com>
 
 import Syn.exceptions
@@ -5,6 +8,11 @@ import Syn.policy.db as D
 import Syn.package_registry
 
 def install(synball):
+	"""
+	Install installs a synball to the syn staging area, making sure to
+	carefully note the status of the package in the pkgdb.
+	@arg synball: Syn tarball to install.
+	"""
 	ROOT_PATH = D.DB_ROOT
 	pkgdb = Syn.package_registry.package_registry(ROOT_PATH)
 
