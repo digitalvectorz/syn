2cf7fb9ca49d74fcf97596c9278a134d94d22d18
Paul Tagliamonte <paultag@ubuntu.com>
2011-08-04 01:06:55 +0000

    trying this bullshit

--- usr-share-syn/synhelper/syn-core.mk
+++ usr-share-syn/synhelper/syn-core.mk
@@ -2,10 +2,12 @@
 # Because fuck you, that's why
 #
 
-MD5SUMFILE=./meta/md5sum.mani
+MD5SUMFOLD=./meta/
+MD5SUMFILE=$(MD5SUMFOLD)md5sum.mani
 
 syn_strip_wd:
 	cd $(S_DESTDIR) && syn-helpers-strip
 
 syn_md5sum_wd:
+	mkdir $(S_BINARY_ROOT)$(MD5SUMFOLD)
 	cd $(S_BINARY_ROOT) && syn-plumbing md5sum-dir ./ $(MD5SUMFILE)
