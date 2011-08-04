e28510805fbf3870d2f1cc872d7ece188f7179e6
Paul Tagliamonte <paultag@ubuntu.com>
2011-08-04 00:03:02 +0000

    log level docs for jdriselvato

--- Syn/log.py
+++ Syn/log.py
@@ -3,13 +3,13 @@
 # GNU GPL-3+, 2011
 #
 
-CRITICAL = 0
-HIGH     = 1
-MESSAGE  = 2
-NOTICE   = 3
-LOG      = 4
-VERBOSE  = 5
-PEDANTIC = 6
+CRITICAL = 0 # So critical, we disturb the user during sex
+HIGH     = 1 # Ignore anyone in the room to get this message
+MESSAGE  = 2 # Watch the spew carefully for these
+NOTICE   = 3 # Cool nugget of information we'd like to let the user know about
+LOG      = 4 # Bare minimum needed to notify someone of the state of the app
+VERBOSE  = 5 # Bare minimum needed to debug the app during normal use
+PEDANTIC = 6 # Know about every call made to any function at all points.
 
 HR_LOGLEVELS = {
 	CRITICAL : "Critical ",
