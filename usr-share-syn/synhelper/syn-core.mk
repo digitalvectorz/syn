d6c2b2cee9e830d4865fc83d36073f1c2e1b323a
Paul Tagliamonte <paultag@ubuntu.com>
2011-08-04 01:16:36 +0000

    humm, cool

--- usr-share-syn/synhelper/syn-core.mk
+++ usr-share-syn/synhelper/syn-core.mk
@@ -9,5 +9,5 @@ syn_strip_wd:
 	cd $(S_DESTDIR) && syn-helpers-strip
 
 syn_md5sum_wd:
-	mkdir $(S_BINARY_ROOT)$(MD5SUMFOLD)
+	mkdir $(S_BINARY_ROOT)/$(MD5SUMFOLD)
 	cd $(S_BINARY_ROOT) && syn-plumbing md5sum-dir ./ $(MD5SUMFILE)
